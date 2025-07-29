41bda1867125f86a377c58419b7ba118a58394d2
yeswanth T <38999685+yeswanth98@users.noreply.github.com>
2025-07-29 19:32:58 +0530

    Add files via upload

---
+++ Day 21 - SOLID principles/HomeTask_Aggregation_Task02.java
@@ -0,0 +1,34 @@
+class Driver {
+    private String name;
+
+    public Driver(String name) {
+        this.name = name;
+    }
+
+    // Getter for name (optional)
+    public String getName() {
+        return name;
+    }
+}
+
+class Car {
+    private Driver driver; // Aggregation
+
+    public Car(Driver driver) {
+        this.driver = driver;
+    }
+
+    // Getter for driver (optional)
+    public Driver getDriver() {
+        return driver;
+    }
+}
+
+public class HomeTask_Task02 {
+    public static void main(String[] args) {
+        Driver driver = new Driver("John");
+        Car myCar = new Car(driver);
+
+        System.out.println("Driver's name: " + myCar.getDriver().getName());
+    }
+}
